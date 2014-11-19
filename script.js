11286d043b14c54415a36aadcd3503a7ccd7bfa2
codingchik <anabel.cuevas4@gmail.com>
2014-11-18 18:31:27 -0800

    added features to contact page

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#departing').datepicker();
+    $('#returning').datepicker();
+    });
+
+
+
+// function changeMe() {
+//   var link = document.getElementById("link");
+//   link.inner
+// }
+
